0a4dc98e360ccc4d59f09c92b892f5ee1fe763da
hameed kazeem <hameed.kazeem@gmail.com>
2020-10-09 21:49:00 -0400

    app post route

--- server.js
+++ server.js
@@ -70,27 +70,28 @@ app.delete("/api/candidate/:id", (req, res) => {
 
 // Create a candidate
 app.post('/api/candidate', (req, res) => {
-    const errors = inputCheck(req.body, 'first_name', 'last_name', 'industry_connected');
-    if (errors) {
-        res.status(400).json({ error: errors });
-        return;
-    }
-    const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-                  VALUES (?,?,?,?)`;
-    const params = [body.id, body.first_name, body.last_name, body.industry_connected];
-    // ES5 function, not arrow function, to use this
-    db.run(sql, params, function (err, result) {
-        if (err) {
-            res.status(500).json({
-                error: err.message
-            });
-        }
-        res.json({
-            message: 'success',
-            data: body,
-            id:this.lastID
-        })
-    });
+    res.json(req.body);
+    // const errors = inputCheck(req.body, 'first_name', 'last_name', 'industry_connected');
+    // if (errors) {
+    //     res.status(400).json({ error: errors });
+    //     return;
+    // }
+    // const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+    //               VALUES (?,?,?,?)`;
+    // const params = [body.id, body.first_name, body.last_name, body.industry_connected];
+    // // ES5 function, not arrow function, to use this
+    // db.run(sql, params, function (err, result) {
+    //     if (err) {
+    //         res.status(500).json({
+    //             error: err.message
+    //         });
+    //     }
+    //     res.json({
+    //         message: 'success',
+    //         data: body,
+    //         id:this.lastID
+    //     })
+    // });
 })
 
 // Default response for any other request(Not Found) Catch all
